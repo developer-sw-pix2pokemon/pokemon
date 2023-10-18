7a38ff8de5f96350e97b0fb560151427b1ac2f28
[SW6] 이선우 <sunwoo40101@gmail.com>
2023-10-18 15:31:04 +0900

    feat: result page design 수정

--- pages/ResultPage.js
+++ pages/ResultPage.js
@@ -1,6 +1,6 @@
 import Component from "../core/Component.js";
-import ColorImage from "../components/ColorImage.js";
-import PokemonName from "../components/PokemonName.js";
+// import ColorImage from "../components/ColorImage.js";
+// import PokemonName from "../components/PokemonName.js";
 
 export default class ResultPage extends Component {
   // 추후 결과값을 불러오는 코드 추가 예정
@@ -14,16 +14,18 @@ export default class ResultPage extends Component {
     return `
       <div class="result">
         <div class="img-compelete">
-          <img id="colorImage" src="">
-          <h1 id="pokemonName"></h1>
+          <img id="colorImage" src="../data/testB/0007.jpg">
+          <h1 id="pokemonName">AAA</h1>
         </div>
       </div>  
     `;
   }
   mounted() {
-    const $colorImage = this.$target.querySelector("#colorImage");
-    new ColorImage($colorImage, this.$props);
-    const $pokemonName = this.$target.querySelector("#pokemonName");
-    new PokemonName($pokemonName, this.$props);
+    // 이부분은 아직은 필요없을 것 같습니다.
+    // const $colorImage = this.$target.querySelector("#colorImage");
+    //new ColorImage($colorImage, this.$props);
+    // 이부분은 create page에서 입력한 name을 가져와주세요!
+    // const $pokemonName = this.$target.querySelector("#pokemonName");
+    // new PokemonName($pokemonName, this.$props);
   }
 }
