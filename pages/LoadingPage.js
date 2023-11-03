d1d944d859273f0bf5d08310549240886c08adc8
soyeon <alththdus1@naver.com>
2023-11-03 22:32:49 +0900

    feat: LoadingPage수정

--- pages/LoadingPage.js
+++ pages/LoadingPage.js
@@ -16,5 +16,26 @@ export default class LoadingPage extends Component {
     `;
   }
 
-  mounted() {}
+  mounted() {
+    this.fetchLoadingState();
+  }
+
+  async fetchLoadingState() {
+    try {
+      const response = await fetch('<<URL>>');
+      const data = await response.json();
+      this.$props.loading = data.loading;
+      if (this.$props.loading) {
+        this.showResultPage();
+      }
+    } catch (error) {
+      console.error('Failed to fetch loading state:', error);
+    }
+  }
+
+  showResultPage() {
+    // Assuming the result page is another component
+    const resultPage = new ResultPage();
+    resultPage.render();
+  }
 }
